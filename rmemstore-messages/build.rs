0b8d2fbcc86108ff132cefbddad7821de9aae87c
kvc0 <3454741+kvc0@users.noreply.github.com>
2024-08-12 01:25:19 +0000

    move protos under messages

--- rmemstore-messages/build.rs
+++ rmemstore-messages/build.rs
@@ -1,17 +1,24 @@
 #[allow(clippy::unwrap_used)]
 fn main() {
-    let proto_dir = "../proto";
+    let proto_dir = "proto";
 
-    prost_build::Config::new()
-        .bytes([
-            ".rmemstore.Value.blob",
-            ".rmemstore.Get.blob",
-            ".rmemstore.Put.key",
-            ".rmemstore.Get.key",
-        ])
-        .protoc_arg("--python_out=pyi_out:../example-python")
-        .out_dir("./src")
-        .compile_protos(&["../proto/rmemstore.proto"], &[proto_dir])
+    let mut config = prost_build::Config::new();
+    config.bytes([
+        ".rmemstore.Value.blob",
+        ".rmemstore.Get.blob",
+        ".rmemstore.Put.key",
+        ".rmemstore.Get.key",
+    ]);
+    config.out_dir("./src");
+
+    if std::env::var("CI").is_ok() {
+        eprintln!("skipping python output");
+    } else {
+        eprintln!("adding python output");
+        config.protoc_arg("--python_out=pyi_out:../example-python/");
+    }
+    config
+        .compile_protos(&["./proto/rmemstore.proto"], &[proto_dir])
         .unwrap();
 
     println!("cargo:rerun-if-changed={proto_dir}");
